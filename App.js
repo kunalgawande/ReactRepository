454b3518ab8a39b0e4430789e4c9475ab8f9803a
KUNAL GAWANDE <kunal_gawande@rediffmail.com>
2025-04-13 21:50:45 +0530

    first git code commit for react

---
+++ App.js
@@ -0,0 +1,5 @@
+
+const heading = React.createElement("h1", {id:"heading"}, "Namaste React From React File");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
